4903469ff9459336faeb2d1b9ac5ab4e9575562b
MATEAV23 <mateogalvarezvic23@gmail.com>
2024-06-02 21:30:45 -0300

    siguiente commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/MATEAV23/TwGo
 
 go 1.22.2
+
+require github.com/aws/aws-lambda-go v1.47.0 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,49 @@
 package main
 
-func main() {
+import (
+	"context"
+	"os"
 
+	"github.com/aws/aws-lambda-go/events"
+	lambda "github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(EjecutoLambda)
+}
+
+func EjecutoLambda(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	var res *events.APIGatewayProxyResponse
+
+	if !ValidoParametros() {
+		res = &events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       "Error en las variables de entorno. debe incluir 'SecretName' , 'BucketName' y 'UrlPrefix' ",
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+		}
+	}
+
+	return res, nil
+
+}
+
+func ValidoParametros() bool {
+	_, traeParametro := os.LookupEnv("SecretName")
+	if !traeParametro {
+		return traeParametro
+	}
+
+	_, traeParametro = os.LookupEnv("BucketName")
+	if !traeParametro {
+		return traeParametro
+	}
+
+	_, traeParametro = os.LookupEnv("UrlPrefix")
+	if !traeParametro {
+		return traeParametro
+	}
+
+	return traeParametro
 }
