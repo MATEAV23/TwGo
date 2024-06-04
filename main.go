4d53b0122d22ad075b6a9f62002d3adfe84a142b
MATEAV23 <mateogalvarezvic23@gmail.com>
2024-06-04 20:21:22 -0300

    Ultimo Commit

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func EjecutoLambda(ctx context.Context, request events.APIGatewayProxyRequest) (
 		return res, nil
 	}
 
-	path := strings.Replace(request.PathParameters["twitter-go"], os.Getenv("UrlPrefix"), "", -1)
+	path := strings.Replace(request.PathParameters["twittergo"], os.Getenv("UrlPrefix"), "", -1)
 
 	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("path"), path)
 	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("method"), request.HTTPMethod)
